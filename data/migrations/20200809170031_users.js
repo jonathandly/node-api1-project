956ac69897288a3ecba0350b5b003f73230238f7
Jonathan Daly <dalyj138@gmail.com>
2020-08-09 18:42:03 -0400

    modified the user schema.

--- data/migrations/20200809170031_users.js
+++ data/migrations/20200809170031_users.js
@@ -1,8 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (users) {
+    users.increments();
 
-exports.up = function(knex) {
-  
+    users.string("name", 255).notNullable();
+    users.text("bio");
+
+    users.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
 };
